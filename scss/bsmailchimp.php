c958d6c7c8a9e3efee3981af8902ee535ed7bd9c
digitalceo <barnabasajayi@gmail.com>
2018-04-03 14:00:51 +0100

    Beggining of Kampus Hub. Upload on 3 April 2018.

---
+++ scss/bsmailchimp.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en" >
+ 
+<head>
+  <meta charset="UTF-8">
+  <title>Bootstrap 4 Mailchimp Form</title>
+   
+   
+  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css'>
+ 
+   
+   
+</head>
+ 
+<body>
+ 
+  <!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+  <form action="WARNING PERSONAL MAILCHIMP INFO" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+      <h2>Subscribe to our mailing list</h2>
+      <div class="indicates-required">
+        <span class="asterisk">*</span> indicates required
+      </div>
+      <!--ROW 1-->
+      <div class="form-row">
+        <!--COL 1 -->
+        <div class="col">
+          <div class="mc-field-group">
+            <label for="mce-EMAIL">
+              Email Address  <span class="asterisk">*</span>
+            </label>
+            <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
+          </div>
+        </div>
+        <!--COL 2 -->
+        <div class="col">
+          <div class="mc-field-group">
+            <label for="mce-FNAME">
+              First Name  <span class="asterisk">*</span>
+            </label>
+            <input type="text" value="" name="FNAME" class="required form-control" id="mce-FNAME">
+          </div>
+        </div>
+      </div>
+      <!-- ROW 2-->
+      <div class="form-row">
+        <!--COL 1 -->
+        <div class="col">
+          <div class="mc-field-group">
+            <label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+            </label>
+            <input type="text" value="" name="LNAME" class="required form-control" id="mce-LNAME">
+          </div>
+        </div>
+        <!--COL 2 -->
+        <div class="col">
+          <div class="mc-field-group">
+            <label for="mce-MMERGE6">Website  <span class="asterisk">*</span>
+            </label>
+            <input type="url" value="" name="MMERGE6" class="required url form-control" id="mce-MMERGE6">
+          </div>
+        </div>
+      </div>
+      <!--VALIDATION -->
+      <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none">
+        </div>
+        <div class="response" id="mce-success-response" style="display:none">
+        </div>
+      </div>
+      <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="WARNING PERSONAL MAILCHIMP INFO" tabindex="-1" value="">
+      </div>
+      <!--SUBMIT BUTTON -->
+      <div class="clear">
+        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="mt-2 button btn btn-primary">
+      </div>
+    </div>
+  </form>
+</div>
+<!--End mc_embed_signup-->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0-next.1/esm/popper.js'></script>
+ 
+   
+ 
+</body>
+ 
+</html>
